26cf51bf564bca50c7deb6a2b39998de779f21b5
SaulTzakumOchoa <cpp.saul@gmail.com>
2020-01-23 22:04:34 -0600

    Changin url for connect with Mongo Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost/cafe'    
 } else{
-    urlDB = 'mongodb://saul_tzakum:_hola_mundo@cluster0-nps0j.mongodb.net/test'
+    urlDB = 'mongodb+srv://saul_tzakum:_hola_mundo@cluster0-nps0j.mongodb.net/cafe'
 }
 // urlDB = 'mongodb+srv://saul_tzakum:_hola_mundo@cluster0-nps0j.mongodb.net/cafe';
 process.env.URLDB = urlDB;
